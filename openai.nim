ebaa1c393ed77a47246dcbb0f622969a1a5cdaff
wick3dr0se <wick3dr0se@protonmail.com>
2023-10-05 04:39:08 +0000

    implementing create image edit dall-e api - wip

--- openai.nim
+++ openai.nim
@@ -1,4 +1,4 @@
-import httpclient, asyncdispatch, json
+import httpclient, asyncdispatch, json, syncio
 
 type
   Completion = object
@@ -26,15 +26,10 @@ type
   Data = object
     url: string
 
-proc newAIClient*(key: string): AsyncHttpClient =
-  return newAsyncHttpClient(
-    headers = newHttpHeaders({
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " & key
-    })
-  )
+proc newAIClient*() {.await.} =
+  await newAsyncHttpClient()
 
-proc chat*(client: AsyncHttpClient, text: string, model = "gpt-3.5-turbo", temperature = 1.0, role = "user"): Future[string] {.async.} =
+proc chat*(client: AsyncHttpClient, apiKey: string, text: string, model = "gpt-3.5-turbo", temperature = 1.0, role = "user"): Future[string] {.async.} =
   let
     body = %*{
       "model": model,
@@ -46,6 +41,10 @@ proc chat*(client: AsyncHttpClient, text: string, model = "gpt-3.5-turbo", tempe
     }
     response = await client.request(
       "https://api.openai.com/v1/chat/completions",
+      headers = newHttpHeaders({
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " & apiKey
+      }),
       httpMethod = HttpPost, body = $body
     )
     jsonObject = parseJson(await response.body)
@@ -53,7 +52,7 @@ proc chat*(client: AsyncHttpClient, text: string, model = "gpt-3.5-turbo", tempe
   
   return c.choices[0].message.content
 
-proc imageGen*(client: AsyncHttpClient, prompt: string, size = "256x256"): Future[string] {.async.} =
+proc imageGen*(client: AsyncHttpClient, apiKey: string, prompt: string, size = "256x256"): Future[string] {.async.} =
   let
     body = %*{
       "prompt": prompt,
@@ -63,6 +62,10 @@ proc imageGen*(client: AsyncHttpClient, prompt: string, size = "256x256"): Futur
     }
     response = await client.request(
       "https://api.openai.com/v1/images/generations",
+      headers = newHttpHeaders({
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " & apiKey
+      }),
       httpMethod = HttpPost, body = $body
     )
     jsonObject = parseJson(await response.body)
@@ -78,3 +81,31 @@ proc imageGen*(client: AsyncHttpClient, prompt: string, size = "256x256"): Futur
   }
   }
   ]#
+
+proc imageEdit*(client: AsyncHttpClient, apiKey: string, url: string, prompt: string, size = "256x256"): Future[string] {.async.} =
+  
+  # this is proabbly all wrong.
+  # trying to pass a png to this proc and edit it, then return the url to output image
+  # the above proc returns a url which i'm trying to accept here and convert to raw bytes
+  let img = await client.get(url)
+  let image = readFile(await img.body)
+
+  let
+    body = %*{
+      "image": @image,
+      "prompt": prompt,
+      #"mask": mask,
+      #"n": 1,
+      "size": size,
+      #"response_format": "url",
+      #"user": "you"
+    }
+    response = await client.request(
+      "https://api.openai.com/v1/images/edits",
+      headers = newHttpHeaders({"Authorization": "Bearer " & apiKey}),
+      httpMethod = HttpPost, body = $body
+    )
+    jsonObject = parseJson(await response.body)
+    i = to(jsonObject, Image)
+    
+  return i.data[0].url
